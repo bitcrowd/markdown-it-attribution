52d88651b8e35455276abd32bed6fcf88006654e
Julian Dobmann <julian@bitcrowd.net>
2020-03-10 18:36:10 +0100

    Mess up order of functions in index.js to satisfy the linter
    This is not the way this should be solved, but reordering functions for
    now to have a green linter.

--- index.js
+++ index.js
@@ -2,83 +2,17 @@ export default function markdownItAttribution(md, options) {
   const attributionPrefix = (options && options.attributionPrefix) || '--';
   let Token;
 
-  function setupBlockquoteRule() {
-    md.core.ruler.after('block', 'attribution', blockquoteRule);
-  }
-
-  function blockquoteRule(state) {
-    // make Token constructor accessible to deeply nested helper functions
-    Token = state.Token;
-
-    const indicePairs = blockquoteIndicePairs(state.tokens);
-
-    indicePairs.forEach((indices) => {
-      const [from, to] = indices;
-      updateBlockquoteTokens(state.tokens, from, to);
-    });
-  }
-
-  function blockquoteIndicePairs(tokens) {
-    const blockquoteOpenIndices = indicesWithTokenType(tokens, 'blockquote_open');
-    const blockquoteCloseIndices = indicesWithTokenType(tokens, 'blockquote_close');
-
-    return zipArrays(blockquoteOpenIndices, blockquoteCloseIndices);
-  }
-
-  function indicesWithTokenType(tokens, tokenType) {
-    const mapped = tokens.map((token, index) => (token.type === tokenType ? index : null));
-    const filtered = mapped.filter((element) => element !== null);
-    return filtered;
-  }
-
-  function updateBlockquoteTokens(tokens, fromIndex, toIndex) {
-    const updatedTokens = blockquoteTokens(tokens, fromIndex, toIndex);
-
-    replaceBlockquoteTokens(tokens, fromIndex, toIndex, updatedTokens);
-  }
-
   function replaceBlockquoteTokens(tokens, fromIndex, toIndex, updatedTokens) {
     const deleteCount = toIndex - fromIndex + 1;
 
     tokens.splice(fromIndex, deleteCount, ...updatedTokens);
   }
 
-  function blockquoteTokens(tokens, fromIndex, toIndex) {
-    const openingToken = tokens[fromIndex];
-    const closingToken = tokens[toIndex];
-
-    const innerTokens = innerBlockquoteTokens(tokens, fromIndex, toIndex);
-
-    return [openingToken, innerTokens, closingToken].flat(2);
-  }
-
-  function innerBlockquoteTokens(tokens, fromIndex, toIndex) {
-    const { level } = tokens[fromIndex];
-    const quoteLines = tokens
-      .slice(fromIndex, toIndex)
-      .filter((token) => token.type === 'inline')
-      .map((token) => token.content.split('\n'))
-      .flat();
-
-    return quoteLines.map((quoteLine) => singleQuoteLineTokens(quoteLine, level));
-  }
-
-  function singleQuoteLineTokens(quoteLine, level) {
-    const trimmedQuoteLine = quoteLine.trimStart();
-    if (trimmedQuoteLine.startsWith(attributionPrefix)) {
-      const quoteLineWithoutPrefix = trimmedQuoteLine.replace(attributionPrefix, '').trimStart();
-      return [
-        citationOpeningToken(level + 1),
-        inlineToken(quoteLineWithoutPrefix, level + 2),
-        citationClosingToken(level + 1),
-      ];
-    }
-
-    return [
-      paragraphOpeningToken(level + 1),
-      inlineToken(quoteLine, level + 2),
-      paragraphClosingToken(level + 1),
-    ];
+  function citationToken(level, nesting) {
+    const token = new Token('paragraph_open', 'cite', nesting);
+    token.level = level;
+    token.block = true;
+    return token;
   }
 
   function citationOpeningToken(level) {
@@ -89,8 +23,8 @@ export default function markdownItAttribution(md, options) {
     return citationToken(level, -1);
   }
 
-  function citationToken(level, nesting) {
-    const token = new Token('paragraph_open', 'cite', nesting);
+  function paragraphToken(level, nesting) {
+    const token = new Token('paragraph_open', 'p', nesting);
     token.level = level;
     token.block = true;
     return token;
@@ -113,15 +47,81 @@ export default function markdownItAttribution(md, options) {
     return token;
   }
 
-  function paragraphToken(level, nesting) {
-    const token = new Token('paragraph_open', 'p', nesting);
-    token.level = level;
-    token.block = true;
-    return token;
+  function indicesWithTokenType(tokens, tokenType) {
+    const mapped = tokens.map((token, index) => (token.type === tokenType ? index : null));
+    const filtered = mapped.filter((element) => element !== null);
+    return filtered;
   }
 
   function zipArrays(array1, array2) {
     return array1.map((element, index) => [element, array2[index]]);
+  }
+
+  function blockquoteIndicePairs(tokens) {
+    const blockquoteOpenIndices = indicesWithTokenType(tokens, 'blockquote_open');
+    const blockquoteCloseIndices = indicesWithTokenType(tokens, 'blockquote_close');
+
+    return zipArrays(blockquoteOpenIndices, blockquoteCloseIndices);
+  }
+
+  function singleQuoteLineTokens(quoteLine, level) {
+    const trimmedQuoteLine = quoteLine.trimStart();
+    if (trimmedQuoteLine.startsWith(attributionPrefix)) {
+      const quoteLineWithoutPrefix = trimmedQuoteLine.replace(attributionPrefix, '').trimStart();
+      return [
+        citationOpeningToken(level + 1),
+        inlineToken(quoteLineWithoutPrefix, level + 2),
+        citationClosingToken(level + 1),
+      ];
+    }
+
+    return [
+      paragraphOpeningToken(level + 1),
+      inlineToken(quoteLine, level + 2),
+      paragraphClosingToken(level + 1),
+    ];
+  }
+
+  function innerBlockquoteTokens(tokens, fromIndex, toIndex) {
+    const { level } = tokens[fromIndex];
+    const quoteLines = tokens
+      .slice(fromIndex, toIndex)
+      .filter((token) => token.type === 'inline')
+      .map((token) => token.content.split('\n'))
+      .flat();
+
+    return quoteLines.map((quoteLine) => singleQuoteLineTokens(quoteLine, level));
+  }
+
+  function blockquoteTokens(tokens, fromIndex, toIndex) {
+    const openingToken = tokens[fromIndex];
+    const closingToken = tokens[toIndex];
+
+    const innerTokens = innerBlockquoteTokens(tokens, fromIndex, toIndex);
+
+    return [openingToken, innerTokens, closingToken].flat(2);
+  }
+
+  function updateBlockquoteTokens(tokens, fromIndex, toIndex) {
+    const updatedTokens = blockquoteTokens(tokens, fromIndex, toIndex);
+
+    replaceBlockquoteTokens(tokens, fromIndex, toIndex, updatedTokens);
+  }
+
+  function blockquoteRule(state) {
+    // make Token constructor accessible to deeply nested helper functions
+    Token = state.Token;
+
+    const indicePairs = blockquoteIndicePairs(state.tokens);
+
+    indicePairs.forEach((indices) => {
+      const [from, to] = indices;
+      updateBlockquoteTokens(state.tokens, from, to);
+    });
+  }
+
+  function setupBlockquoteRule() {
+    md.core.ruler.after('block', 'attribution', blockquoteRule);
   }
 
   setupBlockquoteRule();
